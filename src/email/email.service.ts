49f308b58e8c83ed5f800e765aa549539aac2496
petergambo <petegambo@gmail.com>
2025-06-14 05:59:13 -0700

    Removed email endpoint from auth guard

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,24 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create-email.dto';
-import { UpdateEmailDto } from './dto/update-email.dto';
+// import { UpdateEmailDto } from './dto/update-email.dto';
 import { sendEmail } from 'src/utils/functions';
 
 @Injectable()
 export class EmailService {
   async send(createEmailDto: CreateEmailDto) {
-    
     await sendEmail(
       `sepconceptz@gmail.com`,
       createEmailDto.message,
       `New contact message.`,
-      `Sparkling Events Contact Form`
-    )
+      `Sparkling Events Contact Form`,
+    );
 
     return await sendEmail(
       createEmailDto.email,
-      createEmailDto.message,
+      `<div>Your message has been received. <br/> ${createEmailDto.message}</div>`,
       `Thank you for contacting us.`,
-      `Sparkling Events Contact Form`
+      `Sparkling Events Contact Form`,
     );
   }
 
@@ -30,9 +30,9 @@ export class EmailService {
     return `This action returns a #${id} email`;
   }
 
-  update(id: number, updateEmailDto: UpdateEmailDto) {
-    return `This action updates a #${id} email`;
-  }
+  // update(id: number, updateEmailDto: UpdateEmailDto) {
+  //   return `This action updates a #${id} email`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} email`;
